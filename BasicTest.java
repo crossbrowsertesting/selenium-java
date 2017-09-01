7de093d8c0da8f1ba16cb063effd3ada8558c8f8
Moo-CBT <stephenm@crossbrowsertesting.com>
2017-09-01 10:34:17 -0500

    Updating Caps to Current version

--- BasicTest.java
+++ BasicTest.java
@@ -33,8 +33,8 @@ class BasicTest {
 
         caps.setCapability("name", "Basic Example");
         caps.setCapability("build", "1.0");
-        caps.setCapability("browser_api_name", "chrome-latest");            // request the latest version of chrome
-        caps.setCapability("os_api_name", "Win10");
+        caps.setCapability("browserName", "Chrome");    // By default this will request the latest version of Chrome        
+        caps.setCapability("platform", "Windows 10");   // To specify version, you'll need setCapability("version", "whatever version"
         caps.setCapability("screen_resolution", "1366x768");
         caps.setCapability("record_video", "true");
         caps.setCapability("record_network", "true");
